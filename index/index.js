ef61ff2bd91b285c1d7ff5e6e89ed62427927378
Heartaster <406321927@qq.com>
2021-10-08 01:41:14 +0800

    Adding eject windows
    1

--- index/index.js
+++ index/index.js
@@ -2,11 +2,11 @@ const app = getApp()
 
 Page({
   data: {
-
+    muti:0
   },
   to_home_name(){
-    wx.navigateTo({
-      url: '../home_name/home_name',
+    this.setData({
+       muti : 1
     })
   },
   to_StandAloneGame(){
@@ -23,7 +23,8 @@ Page({
     console.log('代码片段是一种迷你、可分享的小程序或小游戏项目，可用于分享小程序和小游戏的开发经验、展示组件和 API 的使用、复现开发问题和 Bug 等。可点击以下链接查看代码片段的详细文档：')
     console.log('https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/devtools.html')
   },
-  
+
+
   
 
 })
